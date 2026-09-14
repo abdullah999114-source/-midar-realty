150152ae5566304cac3b6de48d35c3e8b592192a
abdullah999114-source <abdullah999114@gmail.com>
2026-09-14 13:32:23 +0300

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
-export const SUPABASE_URL = "https://rzeaobjwizyyprtjaxft.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_S1ljYLwmuO4tL3DVHpkPeQ_RcuXFtWI";
+export const SUPABASE_URL = "https://ifephdnhsjlcpxmcoqfj.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_ek6L3LSyGBXW7i7iL9RuIg_rbkZTXDD";
 export const SITE_URL = "https://midarrealty.com";
 export const WHATSAPP_NUMBER = "966596598878";
